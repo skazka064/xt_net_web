4187ec9bb7a0f3a9f91140fb217c9f80107b1658
SB <skazka064@mail.ru>
2020-05-20 10:16:38 +0400

    Task 1.1.2

---
+++ Solutions/1/Task 1.2/Triangle/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Triangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int i = 0;
+          
+            while(i < n){                
+                    
+                Console.Write("*");
+
+                i++;
+            
+            }
+                Console.WriteLine("");
+            
+
+        }
+    }
+}
