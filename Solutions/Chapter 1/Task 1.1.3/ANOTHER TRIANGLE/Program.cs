6d798310acc77fa75569bd2dae97be6a9aba94f8
Sergey <skazka064@mail.ru>
2020-05-21 17:01:45 +0300

    6

--- Solutions/Chapter 1/Task 1.1.3/ANOTHER TRIANGLE/Program.cs
+++ Solutions/Chapter 1/Task 1.1.3/ANOTHER TRIANGLE/Program.cs
@@ -10,6 +10,24 @@ namespace ANOTHER_TRIANGLE
     {
         static void Main(string[] args)
         {
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (i < j)
+                    {
+                        Console.Write("");
+
+                    }
+                    else
+                    {
+                        Console.Write("*");
+                    }
+                }
+                Console.WriteLine();
+
+            }
         }
     }
 }
