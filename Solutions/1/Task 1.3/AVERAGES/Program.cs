bd59c019aa619e66dbaef0001b7e21176f6ac4f4
Sergey <skazka064@mail.ru>
2020-05-21 15:13:38 +0300

    1

---
+++ Solutions/1/Task 1.3/AVERAGES/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AVERAGES
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
