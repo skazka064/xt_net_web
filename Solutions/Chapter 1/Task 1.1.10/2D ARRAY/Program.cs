b19d34365a5804477ea09c0f061409edc203fcb8
Sergey <skazka064@mail.ru>
2020-05-22 16:08:00 +0300

    12

---
+++ Solutions/Chapter 1/Task 1.1.10/2D ARRAY/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2D_ARRAY
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] array = new int[,] { { 1, 2 ,3} , { 4, 5,6 } };
+            int count = 0;
+            Console.Write(array.Rank);
+            Console.WriteLine();
+            Console.WriteLine();
+
+            for (int i = 0; i<array.Length; i++)
+            {
+                for(int j =0; j<array.Length; j++)
+                {
+                    
+
+                        Console.Write(j);
+                        Console.WriteLine();
+                    
+                }
+
+            }
+        }
+    }
+}
