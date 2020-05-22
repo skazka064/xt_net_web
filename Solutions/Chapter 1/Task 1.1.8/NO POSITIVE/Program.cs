ba61ba94876c11d15183723c052e2b7931b1e828
Sergey <skazka064@mail.ru>
2020-05-22 14:57:54 +0300

    11

--- Solutions/Chapter 1/Task 1.1.8/NO POSITIVE/Program.cs
+++ Solutions/Chapter 1/Task 1.1.8/NO POSITIVE/Program.cs
@@ -11,13 +11,42 @@ namespace NO_POSITIVE
         static void Main(string[] args)
         {
 
-int[,,] array = new int[,,] { { { 1, 2, 3 }, { 4, 5, 6 },{ -7,-8,-9} }  ,    { { 7, 8, 9 }, { 10, 11, 12 },{ -10,-11,-12} }  ,  { { 7, 8, 9 }, { 10, 11, 12 }, { -10, -11, -12 } } };
+int[,,] array = new int[,,] { { { 1, 2, 3 }, { 4, 5, 6 },{ -7,-8,-9} }  ,    { { 7, 8, 9 }, { 10, 11, 12 },{ -10,-11,-12} }  ,  { { 7, 8, 9 }, { 10 , 11, 12 }, { -10, -11, -12 } } };
 
             Console.Write(array.Rank);
             Console.WriteLine();
-    // Проходим слой
-    
+    // Проходим слой за слоем
+    for (int i =0; i < 3; i++)
+            {
+                  for(int j =0; j<3; j++)
+                {
+                    for(int k =0; k<3; k++)
+                    {
 
+
+                        if (array[i,  j,  k] > 0)
+                        {
+                            array[i, j, k] = 0;
+                        }
+                    
+                    }
+                     
+                }
+            }
+
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    for (int k = 0; k < 3; k++)
+                    {
+                        Console.Write(array[i, j, k]);
+                        Console.WriteLine();
+                    }
+
+                }
+            }
 
 
         }
