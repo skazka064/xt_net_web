d7be49fc32e5c767cec004e8662acd849667725d
Sergey <skazka064@mail.ru>
2020-05-22 12:59:22 +0300

    10

---
+++ Solutions/Chapter 1/Task 1.1.8/NO POSITIVE/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NO_POSITIVE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+int[,,] array = new int[,,] { { { 1, 2, 3 }, { 4, 5, 6 },{ -7,-8,-9} }  ,    { { 7, 8, 9 }, { 10, 11, 12 },{ -10,-11,-12} }  ,  { { 7, 8, 9 }, { 10, 11, 12 }, { -10, -11, -12 } } };
+
+            Console.Write(array.Rank);
+            Console.WriteLine();
+    // Проходим слой
+    
+
+
+
+        }
+    }
+}
