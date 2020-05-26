87994ea3c24322aeb8973166f4c832898f608cac
SB <skazka064@mail.ru>
2020-05-26 18:02:11 +0400

    In Home2

---
+++ Solutions/Chapter 1/Task 1.2.1/AVERAGES/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AVERAGES
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
