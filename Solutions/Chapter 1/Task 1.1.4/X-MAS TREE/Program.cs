c455675c69986b94a09af9defed927e0f6baaf3f
Sergey <skazka064@mail.ru>
2020-05-26 17:04:35 +0300

    3334

---
+++ Solutions/Chapter 1/Task 1.1.4/X-MAS TREE/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace X_MAS_TREE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int N = int.Parse(Console.ReadLine());
+                // int k = 1;
+                for (int i = 1; i <= N; i++)
+                {
+                    for (int k = 1; k <= 2 * i; k += 2)
+                    {
+                        for (int j = N; j >= k / 2 + 2; j--)
+                        {
+                            Console.Write(' ');
+                        }
+
+                        for (int j = 1; j <= k; j++)
+                        {
+                            Console.Write('*');
+                        }
+
+                        Console.WriteLine();
+                    }
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Ошибка");
+            }
+
+        }
+    }
+}
--- Solutions/Chapter 1/Task 1.1.6/FONT ADJUSTMENT/Program.cs
+++ Solutions/Chapter 1/Task 1.1.6/FONT ADJUSTMENT/Program.cs
@@ -17,11 +17,11 @@ namespace FONT_ADJUSTMENT
             None = 0,
             Bold = 1,
             Italic = 2,
-            Underline = 3
+            Underline = 4
         }
         static void Main(string[] args)
         {
-            Console.WriteLine("Параметры надписи: None" );
+            Console.WriteLine("Параметры надписи: None");
             Console.WriteLine();
             Console.WriteLine("Введите: ");
             Console.WriteLine();
@@ -29,22 +29,23 @@ namespace FONT_ADJUSTMENT
             Console.WriteLine();
             Console.WriteLine("2: italic");
             Console.WriteLine();
-            Console.WriteLine("3: underline");
+            Console.WriteLine("4: underline");
             Console.WriteLine();
 
 
 
             //Переменная в которую пишем значения шрифтов
-            Fonts font = 0;
+            Fonts a = 0;
+
 
 
             while (true)
             {
                 //Считываем значение с клавиатуры
-                String input = Console.ReadLine();
-                int a = Int32.Parse(input);
+                int input = Int32.Parse(Console.ReadLine());
 
-                Console.WriteLine("Параметры надписи: " + input);
+
+
                 Console.WriteLine();
                 Console.WriteLine("Введите: ");
                 Console.WriteLine();
@@ -52,32 +53,126 @@ namespace FONT_ADJUSTMENT
                 Console.WriteLine();
                 Console.WriteLine("2: italic");
                 Console.WriteLine();
-                Console.WriteLine("3: underline");
+                Console.WriteLine("4: underline");
                 Console.WriteLine();
 
 
                 //Производим проверку
 
-                switch (a)
+                switch (input)
                 {
                     case 1:
-                        font = Fonts.Bold;
+
+                        if (a.HasFlag(Fonts.Italic))
+                        {
+                            a = Fonts.Bold | Fonts.Italic;
+                        }
+                        else if (a.HasFlag(Fonts.Underline))
+                        {
+                            a = Fonts.Bold | Fonts.Underline;
+                        }
+                        else if (a.HasFlag(Fonts.Bold))
+                        {
+                            a = Fonts.None;
+                        }
+                        else if (a.HasFlag(Fonts.Bold) & a.HasFlag(Fonts.Italic) & a.HasFlag(Fonts.Underline))
+                        {
+                            a = Fonts.Italic | Fonts.Underline;
+                        }
+                        else if (a.HasFlag(Fonts.Bold) & a.HasFlag(Fonts.Italic))
+                        {
+                            a = Fonts.Italic;
+                        }
+                        else if (a.HasFlag(Fonts.Bold) & a.HasFlag(Fonts.Underline))
+                        {
+                            a = Fonts.Underline;
+                        }
+                        else if (a.HasFlag(Fonts.Italic) & a.HasFlag(Fonts.Underline))
+                        {
+                            a = Fonts.Underline | Fonts.Italic | Fonts.Bold;
+                        }
+
+
+                        else a = Fonts.Bold;
                         break;
 
                     case 2:
-                        font = Fonts.Italic;
+
+                        if (a.HasFlag(Fonts.Italic))
+                        {
+                            a = Fonts.None;
+                        }
+                        else if (a.HasFlag(Fonts.Underline))
+                        {
+                            a = Fonts.Italic | Fonts.Underline;
+                        }
+                        else if (a.HasFlag(Fonts.Bold))
+                        {
+                            a = Fonts.Bold | Fonts.Italic;
+                        }
+                        else if (a.HasFlag(Fonts.Bold) & a.HasFlag(Fonts.Italic) & a.HasFlag(Fonts.Underline))
+                        {
+                            a = Fonts.Bold | Fonts.Underline;
+                        }
+                        else if (a.HasFlag(Fonts.Bold) & a.HasFlag(Fonts.Italic))
+                        {
+                            a = Fonts.Bold;
+                        }
+                        else if (a.HasFlag(Fonts.Bold) & a.HasFlag(Fonts.Underline))
+                        {
+                            a = Fonts.Underline | Fonts.Bold | Fonts.Italic;
+                        }
+                        else if (a.HasFlag(Fonts.Italic) & a.HasFlag(Fonts.Underline))
+                        {
+                            a = Fonts.Underline;
+                        }
+
+
+                        else a = Fonts.Italic;
+
                         break;
 
-                    case 3:
-                        font = Fonts.Underline;
+                    case 4:
+                        if (a.HasFlag(Fonts.Italic))
+                        {
+                            a = Fonts.Italic | Fonts.Underline;
+                        }
+                        else if (a.HasFlag(Fonts.Underline))
+                        {
+                            a = Fonts.None;
+                        }
+                        else if (a.HasFlag(Fonts.Bold))
+                        {
+                            a = Fonts.Bold | Fonts.Underline;
+                        }
+                        else if (a.HasFlag(Fonts.Bold) & a.HasFlag(Fonts.Italic) & a.HasFlag(Fonts.Underline))
+                        {
+                            a = Fonts.Bold | Fonts.Italic;
+                        }
+                        else if (a.HasFlag(Fonts.Bold) & a.HasFlag(Fonts.Italic))
+                        {
+                            a = Fonts.Bold | Fonts.Italic | Fonts.Underline;
+                        }
+                        else if (a.HasFlag(Fonts.Bold) & a.HasFlag(Fonts.Underline))
+                        {
+                            a = Fonts.Bold;
+                        }
+                        else if (a.HasFlag(Fonts.Italic) & a.HasFlag(Fonts.Underline))
+                        {
+                            a = Fonts.Italic;
+                        }
+
+
+                        else a = Fonts.Underline;
                         break;
 
                     default:
-                        font = Fonts.None;
+                        a = Fonts.None;
                         break;
                 }
 
-                Console.Write(font);
+                Console.Write("Параметры надписи: ");
+                Console.Write(a);
                 Console.WriteLine();
 
             }
