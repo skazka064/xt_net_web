8c640563d4bebecc73df2d299c80461ba665478e
SB <skazka064@mail.ru>
2020-05-26 20:42:03 +0400

    In Home 1.2.1

--- Solutions/Chapter 1/Task 1.2.1/AVERAGES/Program.cs
+++ Solutions/Chapter 1/Task 1.2.1/AVERAGES/Program.cs
@@ -10,6 +10,17 @@ namespace AVERAGES
     {
         static void Main(string[] args)
         {
+            String str = "Викентий хорошо отметил день рождения: покушал пиццу, посмотрел кино, пообщался со студентами в чате";
+
+            String[] words = str.Split( new char[] {' ', ','});
+            int count = 0;
+            foreach (var value in words)
+            {
+                count = count + value.Length;
+                Console.WriteLine(value);
+            }
+          
+            Console.WriteLine(count/14);
         }
     }
 }
