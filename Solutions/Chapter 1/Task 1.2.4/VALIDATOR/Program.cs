f2dcb63cec80b6f92b2d51c167b3ac63e7c2e57a
Sergey <skazka064@mail.ru>
2020-05-27 13:57:29 +0300

    1.2.4 ok)

---
+++ Solutions/Chapter 1/Task 1.2.4/VALIDATOR/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VALIDATOR
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "я плохо учил русский язык. забываю начинать предложения с заглавной. хорошо, что можно написать программу!";
+            string[] separators = { ".","?","!"};
+            string[] offers = s.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+           
+
+            for (int i =0; i< offers.Length; i++)
+            {
+                offers[i] = offers[i].Trim();
+                offers[i] = (string)(offers[i].Remove(1, offers[i].Length - 1)).ToUpper() + offers[i].Remove(0, 1);
+                Console.WriteLine(offers[i]);
+                Console.WriteLine();
+               
+                
+                
+            }
+
+        }
+    }
+}
