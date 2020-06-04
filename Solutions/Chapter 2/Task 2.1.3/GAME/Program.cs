cb30f9c509af7a66844f8e95b83a33898025b4a8
Sergey <skazka064@mail.ru>
2020-06-04 16:18:16 +0300

    Game Begin

--- Solutions/Chapter 2/Task 2.1.3/GAME/Program.cs
+++ Solutions/Chapter 2/Task 2.1.3/GAME/Program.cs
@@ -4,15 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CUSTOM_PAINT
+namespace GAME
 {
     class Program
     {
         static void Main(string[] args)
         {
-
         }
     }
-
-
 }
