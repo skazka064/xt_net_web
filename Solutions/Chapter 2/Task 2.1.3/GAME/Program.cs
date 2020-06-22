45d6343a604c97471039b24267c5655ea8d39f53
Sergey <skazka064@mail.ru>
2020-06-22 16:34:50 +0300

    Add get set

--- Solutions/Chapter 2/Task 2.1.3/GAME/Program.cs
+++ Solutions/Chapter 2/Task 2.1.3/GAME/Program.cs
@@ -10,8 +10,8 @@ namespace GAME
     {
         static void Main(string[] args)
         {
-
-
+            var player = new Player(1,1);
+            var monster = new Monster(2, 2);
 
         }
     }
@@ -45,6 +45,26 @@ namespace GAME
     }
     public class Monster
     {
+        private int _x;
+
+        public int X
+        {
+            get { return _x; }
+            set { _x = value; }
+        }
+        
+        private int _y;
+                public int Y
+        {
+            get { return _y; }
+            set { _y = value; }
+        }
+
+        public Monster(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
        public void Finding()
         {
                 Console.WriteLine("Я ищу Игрока");
@@ -53,7 +73,7 @@ namespace GAME
         
     }
 
-    public class Stone
+    public class Stone 
     {
         // Препятствия
     }
