c5c0546f250ff62b7412e8e8788c70f1d728c57f
SB <skazka064@mail.ru>
2020-07-05 23:55:56 +0400

    1

--- Solutions/Chapter 3/WEAKEST LINK/Program.cs
+++ Solutions/Chapter 3/WEAKEST LINK/Program.cs
@@ -2,66 +2,44 @@
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace WEAKEST_LINK
 {
 	class Program
     {
-        static void Main(string[] args)
+
+        public static void Main()
         {
-            Logic play = new Logic();
+            Console.WriteLine("Введите N:");
+            int n = Convert.ToInt32(Console.ReadLine()) ;
+            Console.WriteLine("ВЫВОД: Сгенерирован круг людей. Начинаем вычеркивать каждого второго.");
 
-            while (true)
+            int k = 2;
+            Queue<int> ring = new Queue<int>(Enumerable.Range(1, n));
+            int iter = 1;
+            while (ring.Count != 1)
             {
-                Console.WriteLine("Please, enter count of people");
-
-                if (int.TryParse(Console.ReadLine(), out int x) && x > 0)
+                
+                for (int i = 1; i <= k; ++i)
                 {
-                    Console.WriteLine("Please, enter number cross out a person");
-
-                    if (int.TryParse(Console.ReadLine(), out int y) && y > 0)
+                    int x = ring.Dequeue();
+                    if (i != k)
                     {
-                        play.PeopleCrossOut(x, y);
+                       
+                       /*if(ring.Count!=1) Console.WriteLine($"Убиваем {x} воина");*/
+                        ring.Enqueue(x);
+                        
                     }
+
+                    
                 }
+                Console.WriteLine("Раунд {0}. Вычеркнут человек. Людей осталось: {1}",iter, ring.Count);
+           iter++;
             }
+            Console.WriteLine("Остался номер {0} ",ring.Dequeue());
+           
         }
     }
+    
 
-    public class Logic
-    {
-        public void PeopleCrossOut(int n, int number)
-        {
-            int round = 1;
-
-            int summOfPeople = n; // Needed to be split variable
-
-            do
-            {
-                for (int i = 1; i <= n; i++)
-                {
-                    if (i % number == 0)
-                    {
-                        --summOfPeople;
-                    }
-                }
-
-                if (summOfPeople == n)
-                {
-                    Console.WriteLine("Невозможно вычеркнуть человека");
-                    summOfPeople = 1;
-                }
-                else
-                {
-                    Console.WriteLine($"Раунд {round}. Вычеркнут человек(-и). Людей осталось: {summOfPeople}");
-                    n = summOfPeople;
-                    round++;
-                    Console.ReadKey(); // In the task writed "Poetapno" to show a result
-                }
-            }
-            while (summOfPeople != 1);
-        }
-    }
 }
