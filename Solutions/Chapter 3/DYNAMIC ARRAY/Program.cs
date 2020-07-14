59ef608ed9c010c588ede8290c12db16e60deb16
SB <skazka064@mail.ru>
2020-07-14 22:19:25 +0400

    1

--- Solutions/Chapter 3/DYNAMIC ARRAY/Program.cs
+++ Solutions/Chapter 3/DYNAMIC ARRAY/Program.cs
@@ -1,36 +1,56 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DYNAMIC_ARRAY
 {
+    public class DynamicArray<T> : IEnumerable<T>, IEnumerable, ICloneable
+    {
+        private T[] Arr { get; set; }
+        public DynamicArray()
+        {
+            Arr = new T[8];
+        }
+        public object Clone()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+
+
     class Program
     {
-        delegate void Message(); // 1. Объявляем делегат
-
+        static void PrintCollection<T>(T[] mass)
+        {
+            Console.WriteLine("Дженерик");
+            Console.WriteLine(mass[0]);
+        }
+        /*static void PrintCollection(int[] mass)
+        {
+            Console.WriteLine("Обычный метод");
+            Console.WriteLine(mass[0]);
+        }*/
         static void Main(string[] args)
         {
-            Message mes; // 2. Создаем переменную делегата
-            if (DateTime.Now.Hour < 12)
-            {
-                mes = GoodMorning; // 3. Присваиваем этой переменной адрес метода
-            }
-            else
-            {
-                mes = GoodEvening;
-            }
-            mes(); // 4. Вызываем метод
-            Console.ReadKey();
-        }
-        private static void GoodMorning()
-        {
-            Console.WriteLine("Good Morning");
-        }
-        private static void GoodEvening()
-        {
-            Console.WriteLine("Good Evening");
+
+            int[] massInt = new int[2];
+            PrintCollection(massInt);
+
         }
     }
 }
