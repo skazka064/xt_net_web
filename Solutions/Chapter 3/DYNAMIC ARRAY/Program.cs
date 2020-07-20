8030afce49726a78d3d94882c95cea654198eaeb
Sergey <skazka064@mail.ru>
2020-07-20 16:28:43 +0300

    111

--- Solutions/Chapter 3/DYNAMIC ARRAY/Program.cs
+++ Solutions/Chapter 3/DYNAMIC ARRAY/Program.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Dynamic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,40 +14,48 @@ namespace DYNAMIC_ARRAY
     public class DynamicArray<T> : IEnumerable<T>, IEnumerable, ICloneable
     {
         private T[] MyArray { get; set; }
+       
+        private int _length;
+        private int _capacity;
 
         public  int Length
         {
-            get
-            {
-                return  MyArray.Length;
-            }
+            get => _length;
+          
             set
             {
-                Length = value;
+                foreach (var item in MyArray)
+                {
+                    _length = _length++;
+                }
             }
             
         }
         public int Capacity
         {
-            get
+            get => _capacity;
+            set
             {
-                return MyArray.Length;
-            }
-             set
-            {
-                if(MyArray == null)
+               if(_capacity < _length)
                 {
-                    MyArray = new T[value];
+                    throw new ArgumentOutOfRangeException("qqqqq");
                 }
-                else if(value>= Length)
+              
+               else if(_capacity >= _length)
                 {
-                    T[] myTempArray = MyArray;
-                    MyArray = new T[value];
-                    myTempArray.CopyTo(MyArray, 0);
-                }
-                else
-                {
-                    throw new ArgumentOutOfRangeException();
+                    T[] myTempArray = new T[_capacity*_capacity];
+                    int i = _capacity;
+                    
+                    foreach (T item in MyArray)
+                    {
+                        myTempArray[i] = item;
+                        i++;
+                    }
+                   
+                    
+                    MyArray = new T[_capacity * _capacity];
+                    myTempArray.CopyTo(MyArray,0);
+
                 }
             }
         }
@@ -55,15 +64,18 @@ namespace DYNAMIC_ARRAY
         {
             MyArray = new T[8];
             Capacity = 8;
+            
         }
         public DynamicArray(int n)
         {
             MyArray = new T[n];
             Capacity = n;
+            
         }
 
         public DynamicArray(IEnumerable<T> myCollection)
         {
+
             int Size = myCollection.Count();
             MyArray = new T[Size];
             int i = 0;
@@ -79,10 +91,16 @@ namespace DYNAMIC_ARRAY
         {
             if(Length == Capacity)
             {
+                T[] myTempArray = new T[Capacity * 2];
+                MyArray.CopyTo(myTempArray, 0);
+                myTempArray[Length] = item;
+                MyArray = myTempArray;
                 Capacity = Capacity * 2;
             }
-
-            MyArray[Length] = item;
+            else if (Capacity > Length)
+            {
+                MyArray[Length] = item;
+            }
             Length++;
 
         }
@@ -92,7 +110,32 @@ namespace DYNAMIC_ARRAY
             {
                 Capacity = Capacity + myCollection.Count;
             }
+            foreach(var myItem in myCollection)
+            {
+                MyArray[Length] = myItem;
+                Length++;
+            }
         }
+        public bool Remove(T myItem)
+        {
+            for(int i =0; i < Length; i++)
+            {
+                if (MyArray[i].Equals(myItem))
+                {
+                    for(int j=i; j<Length -1; j++)
+                    {
+                        MyArray[i] = MyArray[j + 1];
+
+                    }
+                    Length = Length - 1;
+                    return true;
+                }
+                
+            }
+            return false;
+        }
+
+
         public object Clone()
         {
             throw new NotImplementedException();
@@ -107,6 +150,29 @@ namespace DYNAMIC_ARRAY
         {
             throw new NotImplementedException();
         }
+        public T this[int index]
+        {
+            get
+            {
+                if (index < -Length || index > Length - 1)
+                    throw new ArgumentOutOfRangeException("ArgumentOutOfRangeException");
+                else
+                    if (index < 0)
+                    return MyArray[index + Length];
+                else
+                    return MyArray[index];
+            }
+            set
+            {
+                if (index < -Length || index > Length - 1)
+                    throw new ArgumentOutOfRangeException("ArgumentOutOfRangeException");
+                else
+                    if (index < 0)
+                    MyArray[index + Length] = value;
+                else
+                    MyArray[index] = value;
+            }
+        }
     }
 
     
@@ -117,13 +183,12 @@ namespace DYNAMIC_ARRAY
         static void Main(string[] args)
         {
 
-            Collection<int> c = new Collection<int>();
-            c.Add(1);
-            DynamicArray<IEnumerable> collection = new DynamicArray<IEnumerable>();
-            Console.WriteLine(collection.Capacity);
 
-            DynamicArray<int> d = new DynamicArray<int>(9);
-            Console.WriteLine(d.Capacity);
+
+            DynamicArray<int> myArr = new DynamicArray<int>();
+
+          
+          
 
         }
     }
