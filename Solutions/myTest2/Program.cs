b6f8ceb53f9d49eb6db1c188c64a256ee2d01d7f
Sergey <skazka064@mail.ru>
2020-07-29 10:46:16 +0300

    Add get set

---
+++ Solutions/myTest2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace myTest2
+{
+    class Program
+    {
+        public static float Mode(float[] array)
+        {
+            var groups = array.GroupBy(v => v);
+            float maxCount = groups.Max(g => g.Count());
+            return groups.First(g => g.Count() == maxCount).Key;
+        }
+        static void Main(string[] args)
+        {
+            float [] teams = { 1, 1, 2, 2, 2, 3 };
+
+            Console.WriteLine(Mode(teams));
+            
+
+        }
+    }
+}
