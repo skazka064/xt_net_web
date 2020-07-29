4038fd232be1b2aa3dda5b787fb187f3c71e5298
Sergey <skazka064@mail.ru>
2020-07-29 09:26:46 +0300

    Add get set

--- Solutions/Chapter 3/3.3.1. SUPER ARRAY/Class1.cs
+++ Solutions/Chapter 3/3.3.1. SUPER ARRAY/Class1.cs
@@ -6,12 +6,7 @@ using System.Threading.Tasks;
 
 namespace _3._3._1.SUPER_ARRAY
 {
-
-    class Program
+    class Class1
     {
-        static void Main(string[] args)
-        {
-
-        }
     }
 }
---
+++ Solutions/Chapter 3/3.3.1. SUPER ARRAY/SuperArray.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3._3._1.SUPER_ARRAY
+{
+    class SuperArray
+    {
+       public static void mySuper(int[] myArray, Func<int, int> function)
+        {
+            if(myArray == null)
+            {
+                throw new ArgumentNullException();
+            }
+            else
+            {
+                for (int i = 0; i < myArray.Length; i++)
+                {
+                    myArray[i] = function(myArray[i]);
+                }
+            }
+        }
+
+
+
+    }
+}
