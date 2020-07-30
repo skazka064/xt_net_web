be767fd108c2c99ba88b04a78f459d1a78acc6e5
Sergey <skazka064@mail.ru>
2020-07-30 17:16:34 +0300

    Add get set

---
+++ Solutions/Chapter 3/3.3.2. SUPER STRING/Class1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace _3._3._2.SUPER_STRING
+{
+    class Class1
+    {
+        public string EnRu(string str)
+        {
+            string  ru =      @"^[А-я]+$";
+            string en =      @"^[A-z]+$";
+            string nu = @"^[0-9]+$";
+            if(Regex.IsMatch(str, ru))
+            {
+                return "This is Rus";
+            }
+            else if(Regex.IsMatch(str, en))
+            {
+                return "This is En";
+            }
+            else if (Regex.IsMatch(str, nu))
+            {
+                return "This is Numeric";
+            }
+            else
+            {
+                return "This is Mix";
+            }
+            {
+
+            }
+        }
+
+    }
+}
--- Solutions/Chapter 3/3.3.2. SUPER STRING/Program.cs
+++ Solutions/Chapter 3/3.3.2. SUPER STRING/Program.cs
@@ -6,10 +6,15 @@ using System.Threading.Tasks;
 
 namespace _3._3._2.SUPER_STRING
 {
+
     class Program
     {
         static void Main(string[] args)
         {
+          string input =   Console.ReadLine();
+            Class1 class1 = new Class1();
+            Console.WriteLine(class1.EnRu(input));
+            
         }
     }
 }
