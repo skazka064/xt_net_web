d9907d4d887f1c735c3a5fbaa3eaf8a7c8da33b5
Sergey <skazka064@mail.ru>
2020-07-31 15:31:51 +0300

    Add get set

---
+++ Solutions/Chapter 3/3.3.3. PIZZA TIME/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3._3._3.PIZZA_TIME
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
