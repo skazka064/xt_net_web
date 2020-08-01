22dcea3b4b79fe2da31bc726db48d3f5b9f2b9c4
SB <skazka064@mail.ru>
2020-08-01 12:38:13 +0400

    11

--- Solutions/Chapter 3/3.3.3. PIZZA TIME/Pizza.cs
+++ Solutions/Chapter 3/3.3.3. PIZZA TIME/Pizza.cs
@@ -2,18 +2,26 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace _3._3._3.PIZZA_TIME
 {
    public  class Pizza
     {
-        delegate void Handler(string message);
-        event Handler Notify;
+        delegate void delegatePizza(string message);
+        event delegatePizza pizzaReady;
         public Pizza()
         {
+            for (int i = 0; i < 100000000; i++)
+            {
+                if (i == 10000000)
+                {
+                    pizzaReady("Пицца готова!");
+
+                }
+            }
             
-            Notify?.Invoke("Ваша пицца готова!");
         }
     }
 }
