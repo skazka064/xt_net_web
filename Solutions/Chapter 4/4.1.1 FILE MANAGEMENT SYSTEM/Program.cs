427698afec0071cc9e75e33881d1f45d80c2bd12
SB <skazka064@mail.ru>
2020-08-01 22:27:58 +0400

    11

---
+++ Solutions/Chapter 4/4.1.1 FILE MANAGEMENT SYSTEM/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4._1._1_FILE_MANAGEMENT_SYSTEM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
