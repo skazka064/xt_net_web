0f8c0ac6f44bea2c0a2f8a519f766c0b46440022
SB <skazka064@mail.ru>
2020-08-01 11:37:42 +0400

    11

---
+++ Solutions/Chapter 3/3.3.3. PIZZA TIME/Pizza.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3._3._3.PIZZA_TIME
+{
+   public  class Pizza
+    {
+        delegate void Handler(string message);
+        event Handler Notify;
+        public Pizza()
+        {
+            
+            Notify?.Invoke("Ваша пицца готова!");
+        }
+    }
+}
---
+++ Solutions/Chapter 3/3.3.3. PIZZA TIME/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3._3._3.PIZZA_TIME
+{
+    class User
+    {
+
+    }
+}
