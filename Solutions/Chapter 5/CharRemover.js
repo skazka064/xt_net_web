d06cca8db1de9e542418eb4eaeaefe6d91fb599e
SB <skazka064@mail.ru>
2020-08-17 23:03:54 +0400

    at Home

--- Solutions/Chapter 5/CharRemover.js
+++ Solutions/Chapter 5/CharRemover.js
@@ -1,17 +1,28 @@
-function removeDuplicate(str)
-{    
-    var words = str.split(' ');
-    var temp ={};
-    var res;
-    words.forEach(function(item,i, arr){
-        item.split('').forEach(function(myChar,j){
-            var myFind = item.indexOf(myChar); 
+'use strict';
+let str = `У попа была собака`;
 
-        }
-    }
+function charRemover(str) {
 
-   
+    str  = str.toLocaleLowerCase();
+    let strChar = str.split(' '); // get array
+    let emptyStr = {};
+    let result; 
 
+    strChar.forEach(function(item){ // get each words
+        item.split('').forEach(function(charItem, index){ // get char in each words
+            let sum = 0; 
+            let foundPos = item.indexOf(charItem); 
+            if (item.indexOf(charItem, index + 1) != -1) { // looking for coincidence
+                emptyStr[charItem] = 1;
+            }        
+        }); 
+    });
+
+    result = str.split('').filter(function (char) { 
+        return !emptyStr[char];
+    }).join('');
+    
+    console.log(result);
 }
 
-console.log(removeDuplicate("У попа была собака"));
+charRemover(str);
