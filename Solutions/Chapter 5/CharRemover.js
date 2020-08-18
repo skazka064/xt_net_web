af67528a5ce341bb37abddc2bd8d4ffe0ae98c58
SB <skazka064@mail.ru>
2020-08-18 09:58:33 +0400

    at Home

--- Solutions/Chapter 5/CharRemover.js
+++ Solutions/Chapter 5/CharRemover.js
@@ -3,9 +3,9 @@ let str = `У попа была собака`;
 
 function charRemover(str) {
 
-    str  = str.toLocaleLowerCase();
+    
     let myStr = str.split(' '); // get array
-    let myNewArr = {};
+    let myNewArr = [];
     let result; 
 
     myStr.forEach(function(getWord){ // Берем каждое слово из строки
@@ -15,7 +15,7 @@ function charRemover(str) {
                 myNewArr[getChar] = 1;
             }        
         }); 
-    });'use strict';
+    });
 
     result = str.split('').filter(function (char) { 
         return !myNewArr[char];
---
+++ Solutions/mySamplies/splice.js
@@ -0,0 +1,7 @@
+let arr = ["Я","изучаю","JavaScript","прямо","сейчас"];
+//arr.splice(1,1);
+//arr.splice(0,3,"Давай","танцевать")
+//let removed = arr.splice(0,2);
+arr.splice(2,0,"сложный","язык");
+//console.log(removed);
+console.log(arr);
