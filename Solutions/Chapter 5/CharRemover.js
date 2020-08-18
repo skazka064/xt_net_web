b702acc1d15b5fd4d37d4a902a638335edaa4390
SB <skazka064@mail.ru>
2020-08-18 09:06:20 +0400

    at Home

--- Solutions/Chapter 5/CharRemover.js
+++ Solutions/Chapter 5/CharRemover.js
@@ -4,21 +4,21 @@ let str = `У попа была собака`;
 function charRemover(str) {
 
     str  = str.toLocaleLowerCase();
-    let strChar = str.split(' '); // get array
-    let emptyStr = {};
+    let myStr = str.split(' '); // get array
+    let myNewArr = {};
     let result; 
 
-    strChar.forEach(function(item){ // Берем каждое слово из строки
-        item.split('').forEach(function(charItem, index){ // Берем каждый символ из слова
+    myStr.forEach(function(getWord){ // Берем каждое слово из строки
+        getWord.split('').forEach(function(getChar, index){ // Берем каждый символ из слова
             
-            if (item.indexOf(charItem, index + 1) != -1) { // Ищем совпадения
-                emptyStr[charItem] = 1;
+            if (getWord.indexOf(getChar, index + 1) != -1) { // Ищем совпадения
+                myNewArr[getChar] = 1;
             }        
         }); 
     });'use strict';
 
     result = str.split('').filter(function (char) { 
-        return !emptyStr[char];
+        return !myNewArr[char];
     }).join('');
     
     console.log(result);
