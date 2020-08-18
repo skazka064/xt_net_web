c1872aee98d0468b91c7475b675e47182779ae8b
SB <skazka064@mail.ru>
2020-08-18 12:45:16 +0400

    at Home

--- Solutions/Chapter 5/MathCalculator.js
+++ Solutions/Chapter 5/MathCalculator.js
@@ -1,1 +1,39 @@
-let str ="3.5 +4*10-5.3 /5 = ";
+var str = '3.5 +4*10-5.3 /5 =';
+var regex = /(\d+\.\d+)|(\d+)|(\+|\-|\/|\*|\=)/g;
+var items = str.match(regex);
+var res = parseFloat(items[0]);
+for(var i =0; i< items.length;i++){
+    switch(items[i]){
+        case "+":
+            {
+                res += parseFloat(items[i + 1]);
+            }
+        break;
+        case "-":
+            {
+                res-=parseFloat(items[i+1])
+            }
+            break;
+        case "*":
+        {
+            res *= parseFloat(items[i+1])
+        }
+        break;
+        case"/":
+        {
+            res /= parseFloat(items[i+1])
+        }
+        break;
+        case "=":
+        {
+          res = res.toFixed(2);
+          console.log(res);
+        }
+        break;
+
+
+    }
+
+    
+}
+
