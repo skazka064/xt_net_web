744c5025acdb3c38939600d6a51e3bd5c2d58c31
SB <skazka064@mail.ru>
2020-08-20 15:00:38 +0400

    at Home

--- Solutions/myTest4(JS)/test.js
+++ Solutions/myTest4(JS)/test.js
@@ -1,7 +1,4 @@
-let users = [
-    {id:1, name: "Вася"},
-    {id:2, name: "Петя"},
-    {id:3, name: "Маша"}
-];
-let someUsers = users.filter(item=>item.id<3);
-console.log(someUsers);
+
+
+if([null,null]==",")
+console.log("a");
