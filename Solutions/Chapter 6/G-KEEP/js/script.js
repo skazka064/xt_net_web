b9216accb18dc4bf5e7d6038899a6d95a27179aa
SB <skazka064@mail.ru>
2020-09-05 12:54:48 +0400

    at Home

--- Solutions/Chapter 6/G-KEEP/js/script.js
+++ Solutions/Chapter 6/G-KEEP/js/script.js
@@ -1,5 +1,5 @@
 
-// ищем элементы
+// Ищем элементы и присваиваем переменным ссылки на них
 var modal = document.getElementById("myModal");
 var addBtn = document.getElementById("myBtn");
 var closeBtn = document.getElementById("closeBtn");
@@ -10,40 +10,40 @@ var modalBody = document.getElementById("modalBody");
 var list = document.querySelector("#list");
 var imgDelete =document.querySelector(".imgDelete");
 
-
+// Собираем заметку при нажатии на +
 mkBtn.onclick = function(){ 
-
 var containerDiv = document.createElement('div');
 var myTitle = modalHead.value;
 var myText = modalBody.value;
 if((myText==false)||(myTitle==false)){alert("Вы не ввели данные"); return false;} 
 //containerDiv.id = modalHead.value; 
 containerDiv.classList.add("containerDiv");  
-
 var temp ="<p class='p-title'>"+myTitle+"</p>"+"<p onclick='myUpdate(this)' class='p-text'>"+myText+"</p>"+'<img onclick="myDelete(this)" id='+myTitle+' class="imgDelete" src="img/delete.png" alt="Удалить заметку" />';
 containerDiv.innerHTML=temp;
 list.append(containerDiv);
-
 modalHead.value="";
 modalBody.value="";
-
 modal.style.display="none";
 }
-
+//Закрытие модального окна
 closeBtn.onclick=function(){
   modal.style.display="none";
 }
+//Открытие модального окна
 addBtn.onclick = function(){
   modal.style.display="block";
 }
+//Закрытие модального окна при нажатии на крестик
 span.onclick= function(){
   modal.style.display="none";
 }
+//Закрытие модального окна при нажатии на область около него
 window.onclick = function(event){
   if(event.target==modal){
     modal.style.display="none";
   }
 }
+//Функция удаления заметки
 function myDelete(el){
   var id =el.id;
   let qua = confirm('Вы действительно хотите удалить заметку?');
