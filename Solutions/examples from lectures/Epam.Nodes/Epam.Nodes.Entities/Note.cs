a9ea1e4e2559b7acac098ed64b2374674b59a594
SB <skazka064@mail.ru>
2020-09-19 09:45:07 +0400

    at Home

---
+++ Solutions/examples from lectures/Epam.Nodes/Epam.Nodes.Entities/Note.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Epam.Nodes.Entities
+{
+    public class Note
+    {
+        public int Id { get; set; }
+
+        public string Text { get; set; }
+
+        public DateTime DateCreation { get; set; }
+    }
+}
