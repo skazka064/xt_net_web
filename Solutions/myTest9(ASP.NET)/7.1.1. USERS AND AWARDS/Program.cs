821a652c146be0ac92632360fd271f3d3c1cf1ce
SB <skazka064@mail.ru>
2020-09-23 19:34:06 +0400

    at Home

---
+++ Solutions/myTest9(ASP.NET)/7.1.1. USERS AND AWARDS/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7._1._1.USERS_AND_AWARDS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
