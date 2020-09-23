1b8e8d55fed4c039bf98188bc8ebc61ff580bcf8
SB <skazka064@mail.ru>
2020-09-23 22:24:00 +0400

    at Home

--- Solutions/myTest9(ASP.NET)/7.1.1. USERS AND AWARDS/Program.cs
+++ Solutions/myTest9(ASP.NET)/7.1.1. USERS AND AWARDS/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,37 @@ namespace _7._1._1.USERS_AND_AWARDS
 {
     class Program
     {
+
+        public static string LocalDataPath => "Data\\";
+        class User
+        {
+            public Guid ID{get; set;}
+            public string Name { get; set; }
+            public string DateOfBirth { get; set; }
+            public  string Age { get; set; }
+            protected User()
+            {
+                ID = Guid.NewGuid();
+            }
+            public User(string name, string dateofbirth,string age): this()
+            {
+                Name = name ?? throw new ArgumentNullException(nameof(name));
+                DateOfBirth = dateofbirth ?? throw new ArgumentNullException(nameof(dateofbirth));
+                Age = age ?? throw new ArgumentNullException(nameof(age));
+            }
+
+        }
+
+        class Award
+        {
+
+        }
+
+
+
         static void Main(string[] args)
         {
+
         }
     }
 }
