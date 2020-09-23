8d4a0c01fab2410b23705c084436053320c308e1
SB <skazka064@mail.ru>
2020-09-23 23:09:07 +0400

    at Home

--- Solutions/myTest9(ASP.NET)/7.1.1. USERS AND AWARDS/Program.cs
+++ Solutions/myTest9(ASP.NET)/7.1.1. USERS AND AWARDS/Program.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,37 +11,54 @@ namespace _7._1._1.USERS_AND_AWARDS
 {
     class Program
     {
-
         public static string LocalDataPath => "Data\\";
-        class User
+        public static IEnumerable<User> GetAllUser()
         {
-            public Guid ID{get; set;}
-            public string Name { get; set; }
-            public string DateOfBirth { get; set; }
-            public  string Age { get; set; }
-            protected User()
-            {
-                ID = Guid.NewGuid();
-            }
-            public User(string name, string dateofbirth,string age): this()
-            {
-                Name = name ?? throw new ArgumentNullException(nameof(name));
-                DateOfBirth = dateofbirth ?? throw new ArgumentNullException(nameof(dateofbirth));
-                Age = age ?? throw new ArgumentNullException(nameof(age));
-            }
-
+            var directory = new DirectoryInfo(Environment.CurrentDirectory + "\\" + "Data");
+            foreach (var file in directory.GetFiles())
+                using (var reader = new StreamReader(file.FullName))
+                    yield return JsonConvert.DeserializeObject<User>(reader.ReadToEnd());
         }
-
-        class Award
+        public static void SaveUser(User user)
         {
-
+            if (user == null)
+            
+                throw new ArgumentNullException(nameof(user));
+            var userName = "User_" + user.ID + ".json";
+            var userStr = JsonConvert.SerializeObject(user);
+            using (var writer = new StreamWriter(LocalDataPath + userName))
+                writer.Write(userStr);
+            
+            
         }
-
-
-
         static void Main(string[] args)
         {
+            var user1 = new User("Sergio", "26.07.1976", "44");
+            SaveUser(user1);
 
         }
     }
+
+    class User
+    {
+        public Guid ID { get; set; }
+        public string Name { get; set; }
+        public string DateOfBirth { get; set; }
+        public string Age { get; set; }
+        protected User()
+        {
+            ID = Guid.NewGuid();
+        }
+        public User(string name, string dateofbirth, string age) : this()
+        {
+            Name = name ?? throw new ArgumentNullException(nameof(name));
+            DateOfBirth = dateofbirth ?? throw new ArgumentNullException(nameof(dateofbirth));
+            Age = age ?? throw new ArgumentNullException(nameof(age));
+        }
+
+         
+    }
+    
+
+    
 }
