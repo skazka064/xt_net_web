68b155f9ab9ffd4c5363a882ebf401e7dc42295d
SB <skazka064@mail.ru>
2021-01-18 21:55:16 +0400

    at Home

---
+++ Solutions/bot.js
@@ -0,0 +1,6 @@
+document.location.href = "http://ppoz-service-bal-01.egron.test:9001/#/administration/details/PKPVD-2021-01-18-000144/history";
+var btn = document.querySelector("button");
+var txtArea = document.getElementsByClassName("non-resizable");
+txtArea[0].value="Ошибка";
+btn.disabled=false;
+btn.click();
