92d03a0ab8187583094f90625844396e83fa4447
Sergey <skazka064@mail.ru>
2021-05-07 15:48:30 +0400

    at Work

--- Solutions/ExampleLectionAsyncAndLazy/Program.cs
+++ Solutions/ExampleLectionAsyncAndLazy/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,5 +12,54 @@ namespace ExampleLectionAsyncAndLazy
         static void Main(string[] args)
         {
         }
+    }
+
+    public class Loader
+    {
+        public UserData[] LoadData()
+        {
+            // Подключаемся к базе
+            // Получение данных с БД
+            return new UserData[]
+            {
+                new UserData(){Name = "Anton", Id = Guid.NewGuid(), Files = new File[]{ } },
+                new UserData(){Name = "Sergio", Id = Guid.NewGuid(), Files = new File[]{ }},
+                new UserData(){Name = "Pet", Id = Guid.NewGuid(), Files = new File[]{ }},
+
+            };
+        }
+
+        public IEnumerable LoadDataOptimized()
+        {
+            var database = new UserData[]{
+                new UserData(){Name = "Anton", Id = Guid.NewGuid(), Files = new File[]{ } },
+                new UserData(){Name = "Sergio", Id = Guid.NewGuid(), Files = new File[]{ }},
+                new UserData(){Name = "Pet", Id = Guid.NewGuid(), Files = new File[]{ }},
+
+            };
+
+            foreach (var item in database)
+            {
+                //  TODO: вытягивание данных с Базы данных
+                yield return item;
+            }
+
+           
+        }
+
+
+    }
+
+    public class UserData
+    {
+        public string Name { get; set; }
+        public Guid Id { get; set; }
+        public IEnumerable<File> Files { get; set; }
+       
+    }
+
+    public class File
+    {
+
     }
 }
